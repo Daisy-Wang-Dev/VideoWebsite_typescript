13d62e0bc5e107724ffa20949881db88c1452f85
Xueying Wang <daiszws19@gmail.com>
2023-05-17 21:39:35 +0100

    Add header component and mobile styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
 import "./Header.scss";
-import BrainflixLogo from "../../assets/logo/BrainFlix-logo.svg"
+import BrainflixLogo from "../../assets/logo/BrainFlix-logo.svg";
+import Avatar from "../../assets/images/Mohan-muruge.jpg"
+import SearchLogo from "../../assets/icons/search.svg"
 
 function Header() {
   return (
     <header className="header">
       <div className="header__logo">
         <a className="header__link" href="../../../public/index.html">
-          <img className="header__logo-img" src={BrainflixLogo} alt="BrainFlix logo" />
+          <img
+            className="header__logo-img"
+            src={BrainflixLogo}
+            alt="BrainFlix logo"
+          />
         </a>
+      </div>  
+      <div className="header__container">
+       <img className="header__avatar header__avatar--bigger-screen" src={Avatar} alt="profile avatar" />
+       <img className="header__search-logo" src={SearchLogo} alt="search icon" />
+        <input className="header__search" type="text" placeholder="Search" name="search" id="search" />
+        <img className="header__avatar" src={Avatar} alt="profile avatar" />
       </div>
+      <button className="header__btn">Upload</button>
     </header>
   );
 }

0d19223d278de5b8b0cd486b3849ffdcad94d25d
Xueying Wang <daiszws19@gmail.com>
2023-05-19 11:18:21 +0100

    Add all active commments and basic styling

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,21 +4,19 @@ import Comment from "../Comment/Comment";
 function Comments({ activeComments }) {
   return (
     <section className="comments">
+        <h2 className="comments__title">{activeComments.length} Comments</h2>
       {activeComments.map((activeComment) => {
-        // const date = new Date(activeComment.timestamp);
-        // const month = date.toLocaleString("default", { month: "2-digit" });
-        // const day = date.toLocaleString("default", { day: "2-digit" });
-        // const year = date.getFullYear();
-        // const readDate = `${day}/${month}/${year}`;
-        // {console.log(activeComment.id);}
-        // console.log(activeComment.name);
-        // console.log(activeComment.timestamp);
-        // console.log(activeComment.comment);
+        const date = new Date(activeComment.timestamp);
+        const month = date.toLocaleString("default", { month: "2-digit" });
+        const day = date.toLocaleString("default", { day: "2-digit" });
+        const year = date.getFullYear();
+        const readDate = `${day}/${month}/${year}`;
+   
 
-        <Comment
+       return <Comment
           key={activeComment.id}
           fullName={activeComment.name}
-          readDate={activeComment.timestamp}
+          readDate={readDate}
           comment={activeComment.comment}
         />;
       })}

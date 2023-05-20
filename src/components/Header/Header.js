e5fe0b31e45170ad2ecf9f138b0ffcc661c73be7
Xueying Wang <daiszws19@gmail.com>
2023-05-20 11:09:10 +0100

    Add responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.scss";
 import BrainflixLogo from "../../assets/logo/BrainFlix-logo.svg";
 import Avatar from "../../assets/images/Mohan-muruge.jpg";
 import SearchLogo from "../../assets/icons/search.svg";
-
+import UploadIcon from "../../assets/icons/upload.svg";
 function Header() {
   return (
     <header className="header">
@@ -31,7 +31,12 @@ function Header() {
           />
           <img className="header__avatar" src={Avatar} alt="profile avatar" />
         </div>
-        <div className="header__container">
+        <div className="header__upload-container">
+        <img
+            className="header__upload-icon"
+            src={UploadIcon}
+            alt="upload icon"
+          />
           <button className="header__btn">UPLOAD</button>     
           <img className="header__avatar header__avatar--bigger-screen" src={Avatar} alt="profile avatar" />
         </div>

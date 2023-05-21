4a37df8b8a8dcfd8a26792e11a01115eccf84968
Xueying Wang <daiszws19@gmail.com>
2023-05-21 12:43:46 +0100

    Add responsive sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,7 +5,7 @@ function SideBar({ Videos, activeVideoID, changeActiveVideo }) {
   const sideBarVideos = Videos.filter((Video) => Video.id !== activeVideoID);
   return (
     <section className="sidebar">
-      <h2 className="sidebar__title">Next Videos</h2>
+      <h2 className="sidebar__title">NEXT VIDEOS</h2>
       {sideBarVideos.map((video) => (
         <Video
           key={video.id}

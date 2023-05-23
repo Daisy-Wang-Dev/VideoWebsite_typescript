2c5c23cb52edf1d91750016b1498b6539fa19dda
Xueying Wang <daiszws19@gmail.com>
2023-05-23 21:48:51 +0100

    Add mobile upload page and styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Header from "./components/Header/Header.js";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 import VideoDetail from "./components/VideoDetail/VideoDetail";
 import SideBar from "./components/SideBar/SideBar";
+import UploadPage from "./pages/UploadPage/UploadPage";
 import { useState } from "react";
+// import Home from "./pages/Home/Home";
 
 function App() {
   const [activeVideoID, setActiveVideoID] = useState(Videos[0].id);
@@ -18,12 +20,12 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-
         <Routes>
-          <Route></Route>
+          {/* <Route path="/" element={<Home/>}/> */}
+          <Route path="upload" element={<UploadPage/>}/>
         </Routes>
       </BrowserRouter>
-      <div className="App">
+      {/* <div className="App">
         <VideoPlayer Videos={Videos} activeVideoID={activeVideoID} />
         <main>
           <div className="video-details-container">
@@ -34,8 +36,8 @@ function App() {
             activeVideoID={activeVideoID}
             changeActiveVideo={changeActiveVideo}
           />
-        </main>
-      </div>
+        </main> */}
+      {/* </div> */}
     </>
   );
 }
---
+++ src/pages/UploadPage/UploadPage.js
@@ -0,0 +1,49 @@
+import "./UploadPage.scss";
+import { Link } from "react-router-dom";
+import UploadPreview from "../../assets/images/Upload-video-preview.jpg";
+import PublishIcon from "../../assets/icons/publish.svg";
+
+function UploadPage() {
+  return (
+    <section className="upload">
+      <h1 className="upload__title">Upload Video</h1>
+      <div className="upload__container">
+        <div className="upload__thumbnail">
+          <h2 className="upload__thumbnail-title">VIDEO THUMBNAIL</h2>
+          <div className="upload__video">
+            <img
+              className="upload__preview"
+              src={UploadPreview}
+              alt="Upload preview"
+            />
+          </div>
+        </div>
+        <form className="upload__form">
+          <label className="upload__form-title" htmlFor="title">
+            TITLE YOUR VIDEO
+          </label>
+          <input
+            type="text"
+            className="upload__name"
+            id="name"
+            placeholder="Add a title to your video"
+          />
+          <label className="upload__form-title" htmlFor="text">ADD A VIDEO DESCRIPTION</label>
+          <textarea
+            className="upload__description"
+            name="description"
+            id="description"
+            placeholder="Add a description to your video"
+          ></textarea>
+        </form>
+        <div className="upload__btns">
+          <img className="upload__icon" src={PublishIcon} alt="Publish icon" />
+          <Link className="upload__publish" to="/">PUBLISH</Link>
+          <Link className="upload__cancel" to="/">CANCEL</Link>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default UploadPage;

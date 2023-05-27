0c531c57d1dfe4ed9c4ddd7759fa2b19a071bead
Xueying Wang <daiszws19@gmail.com>
2023-05-27 22:34:12 +0100

    Prettify codes

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -39,19 +39,18 @@ function UploadPage() {
           ></textarea>
         </form>
       </div>
-        <div className="upload__btns">
-          <img className="upload__icon" src={PublishIcon} alt="Publish icon" />
-          <Link className="upload__publish" to="/">
-            PUBLISH
-          </Link>
-          <Link className="upload__cancel" to="/">
-            CANCEL
-          </Link>
-          <Link className="upload__publish--bigscreen" to="/">
-            PUBLISH
-          </Link>
-        </div>
-      
+      <div className="upload__btns">
+        <img className="upload__icon" src={PublishIcon} alt="Publish icon" />
+        <Link className="upload__publish" to="/">
+          PUBLISH
+        </Link>
+        <Link className="upload__cancel" to="/">
+          CANCEL
+        </Link>
+        <Link className="upload__publish--bigscreen" to="/">
+          PUBLISH
+        </Link>
+      </div>
     </section>
   );
 }
